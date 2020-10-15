aa205490c0fa267dd2ad2ffe1600715566fc6755
RICARDO ALMEIDA DA ROCHA <ricardoalmeida.for@gmail.com>
2020-10-15 12:24:51 -0300

    usando ORM, criando relacionamentos

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,11 +1,11 @@
-import express from 'express'
+import express from 'express';
+
+import './database/connection';
+import routes from './routes'
 
 const app = express();
 app.use(express.json());
-
-app.get('/users', (request, response) => {
-    return response.json({message: 'Hello World'});
-})
+app.use(routes);
 
 // listening localhost:3333
 app.listen(3333);
