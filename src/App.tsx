dad5729854555c2e1692d787796f45bf21d7b058
RICARDO ALMEIDA DA ROCHA <ricardoalmeida.for@gmail.com>
2020-10-15 01:00:13 -0300

    criado Landing & OrphanagesMap

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,9 @@
 import React from 'react';
-
-interface TitleProps {
-  text: string;
-}
-
-function Title(props: TitleProps){
-  return (
-  <h1>{props.text}</h1>
-  )
-}
+import Routes from './routes';
 
 function App() {
   return (
-    <div className="App">
-      <Title text=" Titulo 1 "/>
-      <Title text=" Titulo 2 "/>
-      <Title text=" Titulo 3 "/>
-      <Title text=" Titulo 4 "/>
-      <Title text=" Titulo 5 "/>
-    </div>
+   <Routes />
   );
 }
 
